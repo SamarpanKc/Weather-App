abaa111cf435967e91e2098bd954407c86dce2b7
Samarpan KC <samarpan0kc@gmail.com>
2024-10-29 11:10:28 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = "78b8e399407393062147676926fe4c0a"; // Your actual API key
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.getElementById("searchInput");
+const searchBttn = document.getElementById("searchBtn");
